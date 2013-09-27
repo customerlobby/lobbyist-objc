4e8449bf2328596a1e30ef5c28e559665e677841
David Lains <dlains@gmail.com>
2013-09-26 20:51:58 -0700

    Initial Commit

---
+++ lobbyist-objc/lobbyist_objc.m
@@ -0,0 +1,13 @@
+//
+//  lobbyist_objc.m
+//  lobbyist-objc
+//
+//  Created by David Lains on 9/26/13.
+//  Copyright (c) 2013 Customer Lobby. All rights reserved.
+//
+
+#import "lobbyist_objc.h"
+
+@implementation lobbyist_objc
+
+@end
